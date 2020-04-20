80b8768a09c64953f2311e4dd89479eafbc5a5e6
Hieu Nguyen <hieunugent@gmail.com>
2020-04-20 15:55:40 -0700

    add insert function

--- BSTProgram.java
+++ BSTProgram.java
@@ -7,8 +7,27 @@ class BSTProgram{
     public BST(int value){
       this.value = value;
     }
+    public BST insert(int value){
+      if (this.value > value){
+        if (left == null){
+          BST newNode = new BST(value);
+          left = newNode;
+        }
+        else{
+          left.insert(value);
+        }
+      }
+      else{
+        if (right == null){
+          BST newNode = new BST(value);
+          right = newNode;
+        }
+        else{
+          right.insert(value);
+        }
+      }
+    }
 
-    
   }
 
 }
